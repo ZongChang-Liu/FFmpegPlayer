8e0cca7185b496e12c15128f99efbf26cb8ca381
Zongchang_Liu <1439797751@qq.com>
2025-06-07 00:20:56 +0800

    add VerticalRulerView for sharing

---
+++ src/Ui/VerticalRulerView.cpp
@@ -0,0 +1,284 @@
+//
+// Created by liu_zongchang on 25-6-6 下午3:23.
+// Email 1439797751@qq.com
+//
+
+#include "VerticalRulerView.h"
+#include <QPainter>
+#include <QDebug>
+#include <QMouseEvent>
+VerticalRulerView::VerticalRulerView(QWidget* parent) : QWidget(parent)
+{
+    this->setMouseTracking(true);
+    m_backgroundRect = QRect(m_pointerSize, m_pointerSize + 1,
+                               width() - 2 - m_pointerSize,
+                               height() - 2 - 2 * m_pointerSize);
+    m_cursorRect = QRect(m_pointerSize, m_pointerSize + 2,
+                               width() - 3 - m_pointerSize,
+                               0);
+    m_lastPos = QPoint(m_backgroundRect.left() + m_backgroundRect.width() / 2, m_backgroundRect.top() + m_backgroundRect.height() - 1);
+    m_mousePos = QPoint(m_backgroundRect.left() + m_backgroundRect.width() / 2, m_backgroundRect.top() + m_backgroundRect.height() - 1);
+    update();
+}
+
+void VerticalRulerView::initRuler(const int minValue, const int maxValue, const int curValue)
+{
+    m_minValue = minValue;
+    m_maxValue = maxValue;
+    m_curValue = curValue;
+
+    double ratio = static_cast<double>(m_curValue - m_minValue) / (m_maxValue - m_minValue);
+    ratio = qBound(0.0, ratio, 1.0); // Ensure ratio is between 0 and 1
+    m_lastPos.setY(static_cast<int>(m_backgroundRect.top() + (1.0 - ratio) * m_backgroundRect.height()));\
+
+
+    if (m_lastPos.y() < m_backgroundRect.top())
+    {
+        m_lastPos.setY(m_backgroundRect.top());
+    }
+    else if (m_lastPos.y() > m_backgroundRect.bottom())
+    {
+        m_lastPos.setY(m_backgroundRect.bottom());
+    }
+    Q_EMIT sigValueChanged(m_curValue);
+    update();
+}
+
+void VerticalRulerView::setThemeMode(const ElaThemeType::ThemeMode themeMode)
+{
+    m_themeMode = themeMode;
+    update();
+}
+
+void VerticalRulerView::resizeEvent(QResizeEvent* event)
+{
+    m_backgroundRect = QRect(m_pointerSize, m_pointerSize + 1,
+                               width() - 2 - m_pointerSize,
+                               height() - 2 - 2 * m_pointerSize);
+
+    m_cursorRect = QRect(m_pointerSize, m_pointerSize + 2,
+                               width() - 3 - m_pointerSize,
+                               m_lastPos.y());
+
+    double ratio = static_cast<double>(m_curValue - m_minValue) / (m_maxValue - m_minValue);
+    ratio = qBound(0.0, ratio, 1.0); // Ensure ratio is between 0 and 1
+    m_lastPos.setY(static_cast<int>(m_backgroundRect.top() + (1.0 - ratio) * m_backgroundRect.height()));
+
+    if (m_lastPos.y() < m_backgroundRect.top())
+    {
+        m_lastPos.setY(m_backgroundRect.top());
+    }
+    else if (m_lastPos.y() > m_backgroundRect.bottom())
+    {
+        m_lastPos.setY(m_backgroundRect.bottom());
+    }
+
+    m_mousePos.setY(m_lastPos.y());
+    m_cursorRect.setHeight(m_lastPos.y() - m_backgroundRect.top());
+
+    QWidget::resizeEvent(event);
+}
+
+void VerticalRulerView::paintEvent(QPaintEvent* event)
+{
+    QWidget::paintEvent(event);
+    QPainter painter(this);
+    painter.setRenderHints(QPainter::Antialiasing, true);
+
+
+    // Draw the control rectangle
+    if (m_isHover)
+    {
+        painter.setPen(QPen(ElaThemeColor(m_themeMode, BasicBorderHover), 1));
+    }
+    else
+    {
+        painter.setPen(QPen(ElaThemeColor(m_themeMode, BasicBorder), 1));
+    }
+    painter.setBrush(ElaThemeColor(m_themeMode, BasicBaseDeep));
+    painter.drawRect(m_backgroundRect);
+
+    //Draw the background
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(ElaThemeColor(m_themeMode, BasicBase));
+    painter.drawRect(m_cursorRect);
+
+
+    // Draw the Scale
+    QFont font;
+    font.setFamily("Microsoft YaHei");
+    font.setPixelSize(10);
+
+    painter.setFont(font);
+    if (m_isHover)
+    {
+        painter.setPen(QPen(ElaThemeColor(m_themeMode, BasicText), 1));
+    } else
+    {
+
+        painter.setPen(QPen(ElaThemeColor(m_themeMode, BasicTextNoFocus), 1));
+    }
+    double startY = m_backgroundRect.top();
+    const double m_tickMarkStep = 1.0 * m_backgroundRect.height() / 100;
+    int num = 100;
+    while (startY <  m_backgroundRect.top() + m_backgroundRect.height())
+    {
+        if (num % 10 == 0 && num != 100 && num != 0)
+        {
+            painter.drawLine(m_pointerSize + 1, startY, m_pointerSize + 16, startY);
+            painter.drawText(m_pointerSize + 21, startY + 4, QString::number(num) + "%");
+        }
+        else if (num % 5 == 0)
+        {
+            painter.drawLine(m_pointerSize + 1, startY, m_pointerSize + 11, startY);
+        }
+        else
+        {
+            painter.drawLine(m_pointerSize + 1, startY, m_pointerSize + 6, startY);
+        }
+        startY += m_tickMarkStep;
+        num--;
+    }
+    painter.drawLine(m_pointerSize, m_backgroundRect.top(), m_pointerSize, m_backgroundRect.bottom());
+    painter.drawLine(m_pointerSize + 1, m_backgroundRect.top(), m_pointerSize + 16, m_backgroundRect.top());
+    painter.drawText(m_pointerSize + 21, m_backgroundRect.top() + 13, "100%");
+    painter.drawLine(m_pointerSize + 1, m_backgroundRect.bottom(), m_pointerSize + 16, m_backgroundRect.bottom());
+    painter.drawText(m_pointerSize + 21, m_backgroundRect.bottom() - 3, "0%");
+
+
+
+    // Draw the pointer
+    painter.setPen(QPen(ElaThemeColor(m_themeMode, StatusDanger), 1.5));
+    painter.setBrush(ElaThemeColor(m_themeMode, StatusDanger));
+    QPolygon triangle;
+    triangle << QPoint(0 , m_mousePos.y() - m_pointerSize / 2)
+             << QPoint(0 , m_mousePos.y() + m_pointerSize / 2)
+             << QPoint(m_pointerSize, m_mousePos.y());
+    painter.drawPolygon(triangle);
+    painter.drawLine(m_pointerSize + 1, m_mousePos.y(), m_pointerSize + m_backgroundRect.width() - 2, m_mousePos.y());
+}
+
+void VerticalRulerView::mousePressEvent(QMouseEvent* event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        m_isPressed = true;
+        if (event->pos().y() < m_backgroundRect.top())
+        {
+            m_lastPos.setY(m_backgroundRect.top());
+        }
+        else if (event->pos().y() > m_backgroundRect.bottom())
+        {
+            m_lastPos.setY(m_backgroundRect.bottom());
+        }
+        else
+        {
+            m_lastPos = event->pos();
+        }
+
+        m_cursorRect.setHeight(m_lastPos.y() - m_backgroundRect.top());
+
+        m_mousePos = m_lastPos;
+        m_curValue = static_cast<int>(m_maxValue - (m_maxValue - m_minValue) * (1.0 * (m_lastPos.y() - m_backgroundRect.top()) / m_backgroundRect.height()));
+
+        update();
+    }
+
+    QWidget::mousePressEvent(event);
+}
+
+void VerticalRulerView::mouseReleaseEvent(QMouseEvent* event)
+{
+    m_isPressed = false;
+    QWidget::mouseReleaseEvent(event);
+}
+
+void VerticalRulerView::mouseMoveEvent(QMouseEvent* event)
+{
+    if (m_backgroundRect.contains(event->pos()))
+    {
+        if (!m_isHover)
+        {
+            m_isHover = true;
+        }
+    }
+    else if (m_isHover)
+    {
+        m_isHover = false;
+    }
+
+    if (m_isPressed)
+    {
+        if (event->pos().y() < m_backgroundRect.top())
+        {
+            m_lastPos.setY(m_backgroundRect.top());
+        }
+        else if (event->pos().y() > m_backgroundRect.bottom())
+        {
+            m_lastPos.setY(m_backgroundRect.bottom());
+        }
+        else
+        {
+            m_lastPos = event->pos();
+        }
+        m_cursorRect.setHeight(m_lastPos.y() - m_backgroundRect.top());
+        m_curValue = static_cast<int>(m_maxValue - (m_maxValue - m_minValue) * (1.0 * (m_lastPos.y() - m_backgroundRect.top()) / m_backgroundRect.height()));
+        Q_EMIT sigValueChanged(m_curValue);
+    }
+
+    if (event->pos().y() < m_backgroundRect.top())
+    {
+        m_mousePos.setY(m_backgroundRect.top());
+    }
+    else if (event->pos().y() > m_backgroundRect.bottom())
+    {
+        m_mousePos.setY(m_backgroundRect.bottom());
+    }
+    else
+    {
+        m_mousePos = event->pos();
+    }
+
+    update();
+    QWidget::mouseMoveEvent(event);
+}
+
+void VerticalRulerView::wheelEvent(QWheelEvent* event)
+{
+    if (event->angleDelta().y() > 0)
+    {
+        m_curValue += 1;
+    }
+    else
+    {
+        m_curValue -= 1;
+    }
+
+
+    if (m_curValue < m_minValue)
+    {
+        m_curValue = m_minValue;
+    }
+    else if (m_curValue > m_maxValue)
+    {
+        m_curValue = m_maxValue;
+    }
+
+    double ratio = static_cast<double>(m_curValue - m_minValue) / (m_maxValue - m_minValue);
+    ratio = qBound(0.0, ratio, 1.0); // Ensure ratio is between 0 and 1
+    m_lastPos.setY(static_cast<int>(m_backgroundRect.top() + (1.0 - ratio) * m_backgroundRect.height()));
+
+
+    if (m_lastPos.y() < m_backgroundRect.top())
+    {
+        m_lastPos.setY(m_backgroundRect.top());
+    }
+    else if (m_lastPos.y() > m_backgroundRect.bottom())
+    {
+        m_lastPos.setY(m_backgroundRect.bottom());
+    }
+    m_cursorRect.setHeight(m_lastPos.y() - m_backgroundRect.top());
+    update();
+    QWidget::wheelEvent(event);
+    event->accept();
+}
---
+++ src/Ui/VerticalRulerView.h
@@ -0,0 +1,51 @@
+//
+// Created by liu_zongchang on 25-6-6 下午3:23.
+// Email 1439797751@qq.com
+//
+
+#ifndef HEIGHT_SCALE_VIEW_H
+#define HEIGHT_SCALE_VIEW_H
+
+#include <QWidget>
+
+#include "Def.h"
+#include "ElaTheme.h"
+
+class VerticalRulerView final : public QWidget {
+    Q_OBJECT
+public:
+    explicit  VerticalRulerView(QWidget *parent = nullptr);
+    ~VerticalRulerView() override = default;
+
+    void initRuler(int minValue, int maxValue, int curValue);
+    Q_SLOT void setThemeMode(ElaThemeType::ThemeMode themeMode);
+
+    Q_SIGNALS:
+    Q_SIGNAL void sigValueChanged(int value);
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
+private:
+    ElaThemeType::ThemeMode m_themeMode = ElaThemeType::Light;
+    int m_pointerSize{10};
+    bool m_isHover{false};
+
+    QRect m_backgroundRect;
+    QRect m_cursorRect;
+
+    bool m_isPressed{false};
+    QPoint m_lastPos;
+    QPoint m_mousePos;
+
+    int m_curValue{0};
+    int m_minValue{0};
+    int m_maxValue{100};
+};
+
+
+
+#endif //HEIGHT_SCALE_VIEW_H
